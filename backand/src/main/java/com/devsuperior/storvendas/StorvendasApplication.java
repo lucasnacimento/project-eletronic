c9fd17f69ab948adbaf4b67b7a7f7d7c979b496e
Lucas <lukasqw4@gmail.com>
2021-09-08 15:08:47 -0300

    Início do projeto

---
+++ backand/src/main/java/com/devsuperior/storvendas/StorvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.storvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorvendasApplication.class, args);
+	}
+
+}
---
+++ backand/src/test/java/com/devsuperior/storvendas/StorvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.storvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
