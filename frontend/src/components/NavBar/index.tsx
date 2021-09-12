413bd2e8a899f0c65081bc8f9563ecef8d9a33a5
Lucas <lukasqw4@gmail.com>
2021-09-12 13:33:20 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,16 @@
 import logoEletronic from 'assets/img/logoEletronic.png';
+import { Link } from 'react-router-dom';
 function NavBar() {
     return (
         <nav className="navbar navbar-dark bg-black">
             <div className="container">
-                <img src={logoEletronic} alt="eletroniclogo" />
-                
+                <Link to="/">
+                    <img src={logoEletronic} alt="eletroniclogo" />
+                </Link>
+
                 <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Digite aqui..." aria-label="Search"/>
-                    <button className="btn btn-warning" type ="submit">Buscar</button>
+                    <input className="form-control me-2" type="search" placeholder="Digite aqui..." aria-label="Search" />
+                    <button className="btn btn-warning" type="submit">Buscar</button>
                 </form>
             </div>
         </nav>
--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -2,7 +2,7 @@ import LogoFooter from 'assets/img/logoEletronicFooter.png';
 function Footer() {
     return (
         <div>
-            <footer className="footer footer-dark bg-black d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <footer className="footer footer-dark bg-black d-flex flex-wrap justify-content-between align-items-center py-5 my-4 border-top">
                 <p className="col-md-4 mb-0 text-white">Sistema Desenvolvido por <a href="https://github.com/luccasNasc" target="_blank" rel="noreferrer">Lucas Nascimento</a> </p>
 
                 <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
